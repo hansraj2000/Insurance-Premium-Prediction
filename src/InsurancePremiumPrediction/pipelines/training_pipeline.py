6ef198ba7e1c3997b28f21a4f5c1e4dbfdd899d6
hansraj2000 <hansrajkr2120@gmail.com>
2024-03-01 14:39:08 +0530

    stop tracking notebooks\data\insurance.csv

--- src/InsurancePremiumPrediction/pipelines/training_pipeline.py
+++ src/InsurancePremiumPrediction/pipelines/training_pipeline.py
@@ -17,12 +17,10 @@ import sys
 
 
 obj=DataIngestion()
-
 train_data_path,test_data_path=obj.initiate_data_ingestion()
 
 data_transformation=DataTransformation()
 train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
-
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
